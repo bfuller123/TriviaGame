1154b701ffc58cdebc8732b5c145479ab347ed49
Brett Fuller <bfuller123@gmail.com>
2017-05-24 23:14:06 -0500

    added html and js skeletons

---
+++ scripts/triviagame.js
@@ -0,0 +1,38 @@
+var questions = {
+  question1: {
+    options:[],
+    answer: null//this will be the answer
+  },
+  question2: {
+    options:[],
+    answer: null//this will be the answer
+  },
+  question3: {
+    options:[],
+    answer: null//this will be the answer
+  },
+  question4: {
+    options:[],
+    answer: null//this will be the answer
+  },
+  question6: {
+    options:[],
+    answer: null//this will be the answer
+  },
+  question7: {
+    options:[],
+    answer: null//this will be the answer
+  },
+  question8: {
+    options:[],
+    answer: null//this will be the answer
+  },
+  question9: {
+    options:[],
+    answer: null//this will be the answer
+  },
+  question10: {
+    options:[],
+    answer: null//this will be the answer
+  }
+};
