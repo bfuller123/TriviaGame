e90a0d6396a4409c8219de07c9b1838d4a3026be
Brett Fuller <bfuller123@gmail.com>
2017-05-26 13:03:56 -0500

    finalized

--- scripts/randomize.js
+++ scripts/randomize.js
@@ -12,7 +12,6 @@ var Randomize = {
   },
   objectKeys: function(object){
     var arrayOfKeys = Randomize.array(Object.keys(object));
-    console.log(arrayOfKeys);
     return arrayOfKeys;
   }
 };
